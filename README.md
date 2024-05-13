3845fa9c7db87dc3f195b4dfd5a0e17786761924
Anuradha Khade <Anuradha.Khade@sainsburys.co.uk>
2024-05-13 21:36:41 +0100

    trial

--- README.md
+++ README.md
@@ -1,2 +1,128 @@
-# cca-gitaction-replace
-trial
+# Replace environment variables in files
+
+## Inputs
+
+- `files`
+  - Path to a file with the placeholders, multiple files, or a directory. If using a directory, all files in the directory will be processed. Glob patterns may be used.
+- `prefix`
+  - Placeholder prefix/suffix. All placeholders are assumed to be contained within `{ }`, with an additional prefix/suffix, e.g. `#{myVar}#`, `${myVar}$`. In these cases, the prefix value would be `#` or `$` respectively.
+  - Optional, defaults to `#`
+- `omit-suffix`
+  - Only use a prefix for the placeholder, do not include a suffix, e.g. `#{myVar}`. Leave this at false for any placeholder containing punctuation, otherwise the placeholder will not be correctly identified.
+  - Optional, defaults to `false`
+
+## Notes
+
+For JSON files, the keys being passed in must reflect the casing of the JSON path that needs to be updated.
+
+For other files, values to replace the placeholders must have the same name as the placeholder, but in UPPERCASE. The casing of the placeholders themselves is not important. Hyphens should not be used, instead, use underscores.
+
+## Example usage
+
+The workflow for a non-JSON file:
+
+```yaml
+name: My actions
+uses: /sainsburys-tech/eo-gitaction-replace@v2
+env:
+  SPECIALFILEPATH: "whatever"
+  SOMETHING_ELSE: "something special"
+with:
+  files: path/to/a/file.txt
+```
+
+The `file`:
+
+```yaml
+env:
+  path: #{specialFilePath}#,
+  other: #{something_else}#
+```
+
+The result:
+
+```yaml
+env:
+  path: whatever
+  other: something special
+```
+
+The workflow for a JSON file:
+
+```yaml
+name: My actions
+uses: /sainsburys-tech/eo-gitaction-replace@v2
+env:
+  AksSettings.Replicas: 3
+  MongoDbSettings.DatabaseName: MyShinyDb
+  SomethingElse: wibble
+with:
+  files: path/to/a/file.json
+```
+
+The `file`:
+
+```json
+{
+  "AksSettings": {
+    "Replicas": "*"
+  },
+  "MongoDbSettings": {
+    "DatabaseName": "*"
+  },
+  "SomethingElse": "*"
+}
+```
+
+The result:
+
+```json
+{
+  "AksSettings": {
+    "Replicas": "3"
+  },
+  "MongoDbSettings": {
+    "DatabaseName": "MyShinyDb"
+  },
+  "SomethingElse": "wibble"
+}
+```
+
+`files` can be any combination of yaml/JSON files. The `env` values must reflect the files being passed in, e.g:
+
+```yaml
+env:
+  PLACEHOLDER_IN_TEXT_FILE: some value
+  PlaceHolder.In.Json.File: some other value
+```
+
+`files` can also be specified as follows:
+
+```yaml
+with:
+  files: |-
+    fileone.yml
+    filetwo.json
+    Dockerfile
+```
+
+```yaml
+with:
+  files: path/to/a/directory
+```
+
+```yaml
+with:
+  files: "dist/**/*.js"
+```
+
+## Note For Developers
+
+If you make any changes to replace/index.js, you will need to recompile the code for it to work as a called action.
+
+Execute the following from the folder containing `packages.json`
+
+```js
+npm i -g @vercel/ncc
+ncc build index.js --license licenses.txt
+```
---
+++ index.js
@@ -0,0 +1,151 @@
+import core from "@actions/core";
+import fs from "fs";
+import fsasync from "fs/promises";
+import path from "path";
+import { set } from "lodash";
+import { globSync } from "glob";
+
+// determine whether the provided path points to a file or a directory that exists
+const checkPath = (path) => {
+  try {
+    const info = fs.statSync(path);
+
+    switch (true) {
+      case info.isDirectory():
+        return "directory";
+      case info.isFile():
+        return "file";
+      default:
+        return "unknown";
+    }
+  } catch (error) {
+    return "unknown";
+  }
+};
+
+const doReplace = (file, prefix, omitSuffix) => {
+  switch (path.extname(file)) {
+    case ".json":
+      doReplaceJson(file);
+      break;
+    default:
+      doReplaceTxt(file, prefix, omitSuffix);
+  }
+};
+
+let paths = [];
+// get an array containing all the json paths in the specified object
+const getPaths = (object, previousPath) => {
+  for (const key in object) {
+    const currentPath = previousPath ? `${previousPath}.${key}` : key;
+
+    if (Array.isArray(object[key])) {
+      paths.push(currentPath);
+      getPaths(object[key], currentPath);
+    } else if (typeof object[key] === "object") {
+      if (!Array.isArray(object)) {
+        paths.push(currentPath);
+      }
+      getPaths(object[key], currentPath);
+    } else {
+      paths.push(currentPath);
+    }
+  }
+};
+
+// replace the values of the json key/value pair with matching
+// values from the env variables
+// env variable case must match the json key case
+const doReplaceJson = async (file) => {
+  const fileContents = await fsasync.readFile(file, "utf-8");
+  const json = JSON.parse(fileContents);
+
+  paths = [];
+  getPaths(json);
+
+  paths.forEach((path) => {
+    // does the variable exist in the env?
+    const env = process.env[path] || "not found";
+
+    if (env !== "not found") {
+      core.info(`Replacing placeholder for ${path}`);
+      set(json, path, env);
+    }
+  });
+
+  await fsasync.writeFile(file, JSON.stringify(json));
+  core.info("File saved");
+};
+
+// replace the placeholders with matching values from the env variables
+// env variables must be all uppercase, placeholder case is unimportant
+const doReplaceTxt = async (file, prefix, omitSuffix) => {
+  const pattern = `${prefix}{.+}${prefix}`;
+  const checkPattern = `${prefix}{.+)}${prefix}`;
+
+  if (omitSuffix === true) {
+    pattern = `${prefix}{\\w+}`;
+    checkPattern = `${prefix}{(\\w+)}`;
+    core.info("omitting suffix");
+  }
+
+  const regExp = new RegExp(pattern, "gi");
+  const check = new RegExp(checkPattern, "i");
+  const fileContents = await fsasync.readFile(file, "utf-8");
+
+  const result = fileContents.replace(regExp, (placeholder) => {
+    // we've found a placeholder, now extract the variable name
+    const varName = placeholder.match(check)[1];
+
+    // does the variable exist in the env?
+    const env = process.env[varName.toUpperCase()];
+
+    if (typeof env === "undefined") {
+      core.warning(`Value missing for ${varName}`);
+
+      return placeholder;
+    } else {
+      core.info(`Replacing placeholder ${placeholder}`);
+    }
+
+    // we're all good, return the value to use as the replacement
+    return env;
+  });
+
+  await fsasync.writeFile(file, result);
+  core.info("File saved");
+};
+
+// main part of code
+try {
+  const pathList = core.getInput("files");
+  const prefix = core.getInput("prefix");
+  const omitSuffix = core.getInput("omit-suffix");
+  let paths;
+
+  if (pathList.includes("*")) {
+    paths = globSync(pathList, { ignore: "node_modules/**" });
+  } else {
+    paths = pathList.split("\n");
+  }
+
+  paths.forEach(async (path) => {
+    if (path.length > 0) {
+      const pathType = checkPath(path);
+
+      switch (pathType) {
+        case "file":
+          doReplace(path, prefix, omitSuffix);
+          break;
+        case "directory":
+          const files = await fsasync.readdir(path);
+          files.forEach((file) => doReplace(file, prefix, omitSuffix));
+          break;
+        default:
+          throw new Error(`invalid or missing path/file: ${path}`);
+      }
+    }
+  });
+} catch (error) {
+  core.setFailed(error.message);
+}
